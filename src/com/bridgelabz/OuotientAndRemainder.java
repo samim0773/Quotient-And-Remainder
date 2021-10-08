4909c9f714d022344636c6486ab1476153ef9d86
Samim Aktar <samimaktar0773@gmail.com>
2021-10-09 00:00:31 +0530

    Done Quotient and remainder

---
+++ src/com/bridgelabz/OuotientAndRemainder.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class OuotientAndRemainder {
+	public static void main(String[] args) {
+		
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter your dividend value: ");
+		
+		int dividend =sc.nextInt();
+		System.out.println("Enter your divisor value: ");
+		
+		
+		int divisor = sc.nextInt();
+
+	    int quotient = dividend / divisor;
+	    int remainder = dividend % divisor;
+
+	    System.out.println("Quotient = " + quotient);
+	    System.out.println("Remainder = " + remainder);
+	}
+
+}
